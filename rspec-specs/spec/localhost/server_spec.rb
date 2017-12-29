901e5393562049569ca4a1f8bda9c0a3bfad6bc3
Oliver Wolf <root@streacs.com>
2017-12-30 00:39:45 +0100

    added more directory tests

--- rspec-specs/spec/localhost/server_spec.rb
+++ rspec-specs/spec/localhost/server_spec.rb
@@ -6,3 +6,15 @@ describe file('/home/fisheye') do
   it { should be_owned_by 'fisheye' }
   it { should be_grouped_into 'fisheye' }
 end
+
+describe file('/opt/atlassian/fisheye') do
+  it { should be_directory }
+  it { should be_owned_by 'fisheye' }
+  it { should be_grouped_into 'fisheye' }
+end
+
+describe file('/var/opt/atlassian/application-data/fisheye') do
+  it { should be_directory }
+  it { should be_owned_by 'fisheye' }
+  it { should be_grouped_into 'fisheye' }
+end
